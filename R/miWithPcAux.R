ea6853d1bc1a6df5bbe322a176b6b9a48c9d84bd
Danny Squire <15035820+dsquire@users.noreply.github.com>
2023-05-20 21:17:30 -0500

    docs: miWithPcAux
    Added documentation structure for function.

--- R/miWithPcAux.R
+++ R/miWithPcAux.R
@@ -1,30 +1,26 @@
-### Title:        Conduct Multiple Imputation with PC Auxiliaries
-### Author:       Kyle M. Lang
-### Contributors: Steven Chesnut, Pavel Panko, Luke Waggenspack
-### Created:      2015-SEP-17
-### Modified:     2017-MAY-25
-### Purpose:      Use the principal component auxiliaries produced by
-###               createPcAux() to conduct MI.
-
-##--------------------- COPYRIGHT & LICENSING INFORMATION --------------------##
-##  Copyright (C) 2018 Kyle M. Lang <k.m.lang@uvt.nl>                         ##
-##                                                                            ##
-##  This file is part of PcAux.                                               ##
-##                                                                            ##
-##  This program is free software: you can redistribute it and/or modify it   ##
-##  under the terms of the GNU General Public License as published by the     ##
-##  Free Software Foundation, either version 3 of the License, or (at you     ##
-##  option) any later version.                                                ##
-##                                                                            ##
-##  This program is distributed in the hope that it will be useful, but       ##
-##  WITHOUT ANY WARRANTY; without even the implied warranty of                ##
-##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General  ##
-##  Public License for more details.                                          ##
-##                                                                            ##
-##  You should have received a copy of the GNU General Public License along   ##
-##  with this program. If not, see <http://www.gnu.org/licenses/>.            ##
-##----------------------------------------------------------------------------##
-
+#' Title
+#'
+#' @param rawData
+#' @param pcAuxData
+#' @param nImps
+#' @param nomVars
+#' @param ordVars
+#' @param idVars
+#' @param dropVars
+#' @param nComps
+#' @param compFormat
+#' @param seed
+#' @param simMode
+#' @param forcePmm
+#' @param nProcess
+#' @param verbose
+#' @param control
+#' @param micemethods
+#'
+#' @return
+#' @export
+#'
+#' @examples
 
 miWithPcAux <- function(rawData,
                         pcAuxData,
