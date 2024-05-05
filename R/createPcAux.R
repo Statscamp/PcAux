da541f812f5e30f77dcea9c316dfaedd84145ab0
Danny Squire <15035820+dsquire@users.noreply.github.com>
2024-05-05 18:20:24 -0500

    Update createPcAux.R

--- R/createPcAux.R
+++ R/createPcAux.R
@@ -57,132 +57,158 @@ createPcAux <- function(pcAuxData,
                         micemethods = c("norm", "polr", "polyreg", "logreg"),
                         ...)
 {
-    pcAuxData$setCall(match.call(), parent = "createPcAux")
+  pcAuxData$setCall(match.call(), parent = "createPcAux")
 
-    ## Set initial time and status check
-    pcAuxData$setTime()
-    if(pcAuxData$checkStatus == "start" | pcAuxData$checkStatus == "all")
-        pcAuxData$setStatus()
+  ## Set initial time and status check
+  pcAuxData$setTime()
+  if (pcAuxData$checkStatus == "start" |
+      pcAuxData$checkStatus == "all")
+    pcAuxData$setStatus()
 
 
-    ## Check for problems with the input values:
-    if(missing(pcAuxData)) errFun("noPcAuxData")
-    if(missing(nComps))    errFun("noNComps")
-    if(!simMode)           checkInputs()
+  ## Check for problems with the input values:
+  if (missing(pcAuxData))
+    errFun("noPcAuxData")
+  if (missing(nComps))
+    errFun("noNComps")
+  if (!simMode)
+    checkInputs()
 
-    ## Add elements to an extant instance of the PcAuxData class:
-    pcAuxData$nComps   <- nComps
-    pcAuxData$forcePmm <- FALSE # Don't give imputation options other than PMM
-    pcAuxData$intMeth  <- as.integer(interactType)
-    pcAuxData$maxPower <- as.integer(maxPolyPow)
-    pcAuxData$simMode  <- simMode
-    pcAuxData$verbose  <- as.integer(verbose)
+  ## Add elements to an extant instance of the PcAuxData class:
+  pcAuxData$nComps   <- nComps
+  pcAuxData$forcePmm <-
+    FALSE # Don't give imputation options other than PMM
+  pcAuxData$intMeth  <- as.integer(interactType)
+  pcAuxData$maxPower <- as.integer(maxPolyPow)
+  pcAuxData$simMode  <- simMode
+  pcAuxData$verbose  <- as.integer(verbose)
 
-    if(!missCheck(seed)) pcAuxData$seed <- as.integer(seed)
+  if (!missCheck(seed))
+    pcAuxData$seed <- as.integer(seed)
 
-    ## Make sure the control list is fully populated:
-    if(!missCheck(control)) pcAuxData$setControl(x = control)
+  ## Make sure the control list is fully populated:
+  if (!missCheck(control))
+    pcAuxData$setControl(x = control)
 
-    pcAuxData$setTime("dataCheck")
-    if(pcAuxData$checkStatus == "all") pcAuxData$setStatus("dataCheck")
+  pcAuxData$setTime("dataCheck")
+  if (pcAuxData$checkStatus == "all")
+    pcAuxData$setStatus("dataCheck")
 
-    ## Check for extant moderators:
-    check <- missCheck(pcAuxData$moderators)
-    if(check) {
-        pcAuxData$moderators <- colnames(pcAuxData$data)
-        if(pcAuxData$intMeth ==1 | pcAuxData$intMeth == 2)
-            warnFun("noMods", map = pcAuxData)
-    }
+  ## Check for extant moderators:
+  check <- missCheck(pcAuxData$moderators)
+  if (check) {
+    pcAuxData$moderators <- colnames(pcAuxData$data)
+    if (pcAuxData$intMeth == 1 | pcAuxData$intMeth == 2)
+      warnFun("noMods", map = pcAuxData)
+  }
 
-    pcAuxData$setTime("modExt")
-    if(pcAuxData$checkStatus == "all") pcAuxData$setStatus("modExt")
+  pcAuxData$setTime("modExt")
+  if (pcAuxData$checkStatus == "all")
+    pcAuxData$setStatus("modExt")
 
-    ## Re-cast the data if needed
-    if(castData | interactType == 1) castData(map = pcAuxData)
+  ## Re-cast the data if needed
+  if (castData | interactType == 1)
+    castData(map = pcAuxData)
 
-    pcAuxData$setTime("cast")
-    if(pcAuxData$checkStatus == "all") pcAuxData$setStatus("cast")
+  pcAuxData$setTime("cast")
+  if (pcAuxData$checkStatus == "all")
+    pcAuxData$setStatus("cast")
 
-    if(doImputation) {
-        ## Check for and treat any nominal variables that are missing only one
-        ## datum:
-        singleMissNom <-
-            with(pcAuxData, (nrow(data) - respCounts == 1) &
-                            (typeVec == "binary" | typeVec == "nominal")
-                 )
-        ## KML 2016-NOV-14: Ignore dropped variables
-        singleMissNom <- setdiff(names(singleMissNom)[singleMissNom],
-                                 pcAuxData$dropVars[ , 1])
+  if (doImputation) {
+    ## Check for and treat any nominal variables that are missing only one
+    ## datum:
+    singleMissNom <-
+      with(pcAuxData,
+           (nrow(data) - respCounts == 1) &
+             (typeVec == "binary" |
+                typeVec == "nominal"))
+    ## KML 2016-NOV-14: Ignore dropped variables
+    singleMissNom <-
+      setdiff(names(singleMissNom)[singleMissNom],
+              pcAuxData$dropVars[, 1])
 
-        if(length(singleMissNom) > 0) pcAuxData$fillNomCell(singleMissNom)
+    if (length(singleMissNom) > 0)
+      pcAuxData$fillNomCell(singleMissNom)
 
-        pcAuxData$setTime("doImp")
-        if(pcAuxData$checkStatus == "all") pcAuxData$setStatus("doImp")
-    }
+    pcAuxData$setTime("doImp")
+    if (pcAuxData$checkStatus == "all")
+      pcAuxData$setStatus("doImp")
+  }
 
-    ## Compute interactions for use during initial imputation:
-    if(pcAuxData$intMeth == 1) {
-        pcAuxData$computeInteract()
-        pcAuxData$data     <- with(pcAuxData, data.frame(data, interact))
-        pcAuxData$interact <- "Removed to save resources"
-    }
+  ## Compute interactions for use during initial imputation:
+  if (pcAuxData$intMeth == 1) {
+    pcAuxData$computeInteract()
+    pcAuxData$data     <-
+      with(pcAuxData, data.frame(data, interact))
+    pcAuxData$interact <- "Removed to save resources"
+  }
 
-    pcAuxData$setTime("compInt")
-    if(pcAuxData$checkStatus == "all") pcAuxData$setStatus("compInt")
+  pcAuxData$setTime("compInt")
+  if (pcAuxData$checkStatus == "all")
+    pcAuxData$setStatus("compInt")
 
-    ## Compute polynomials for use during initial imputation:
-    if(pcAuxData$maxPower > 1) {
-        pcAuxData$computePoly()
-        pcAuxData$data <- with(pcAuxData, data.frame(data, poly))
-        if(pcAuxData$intMeth == 1) pcAuxData$poly <- "Removed to save resources"
-    }
+  ## Compute polynomials for use during initial imputation:
+  if (pcAuxData$maxPower > 1) {
+    pcAuxData$computePoly()
+    pcAuxData$data <- with(pcAuxData, data.frame(data, poly))
+    if (pcAuxData$intMeth == 1)
+      pcAuxData$poly <- "Removed to save resources"
+  }
 
-    pcAuxData$setTime("compPoly")
-    if(pcAuxData$checkStatus == "all") pcAuxData$setStatus("compPoly")
+  pcAuxData$setTime("compPoly")
+  if (pcAuxData$checkStatus == "all")
+    pcAuxData$setStatus("compPoly")
 
-    ## Execute the initial, single imputation:
-    if(doImputation) {
+  ## Execute the initial, single imputation:
+  if (doImputation) {
+    doSingleImputation(map = pcAuxData, micemethods = micemethods)
 
-        doSingleImputation(map = pcAuxData, micemethods = micemethods)
+    ## Use imputed data to update nominal variable representations:
+    if (length(pcAuxData$nomVars) > 0)
+      pcAuxData$codeNomVars()
 
-        ## Use imputed data to update nominal variable representations:
-        if(length(pcAuxData$nomVars) > 0) pcAuxData$codeNomVars()
+    pcAuxData$setTime("doSingle")
+    if (pcAuxData$checkStatus == "all")
+      pcAuxData$setStatus("doSingle")
+  }
 
-        pcAuxData$setTime("doSingle")
-        if(pcAuxData$checkStatus == "all") pcAuxData$setStatus("doSingle")
-    }
+  ## Extract the linear principal component scores:
+  doPCA(map = pcAuxData)
 
-    ## Extract the linear principal component scores:
-    doPCA(map = pcAuxData)
+  pcAuxData$setTime("doPCA")
+  if (pcAuxData$checkStatus == "all")
+    pcAuxData$setStatus("doPCA")
 
-    pcAuxData$setTime("doPCA")
-    if(pcAuxData$checkStatus == "all") pcAuxData$setStatus("doPCA")
+  ## Are we constructing seperate non-linear PcAux?
+  if (pcAuxData$nComps[2] != 0) {
+    ## Undo dummy coding to facilitate interaction calculation:
+    if (!missCheck(pcAuxData$nomVars))
+      pcAuxData$castNomVars(toNumeric = FALSE)
 
-    ## Are we constructing seperate non-linear PcAux?
-    if(pcAuxData$nComps[2] != 0) {
-        ## Undo dummy coding to facilitate interaction calculation:
-        if(!missCheck(pcAuxData$nomVars))
-            pcAuxData$castNomVars(toNumeric = FALSE)
-
-        ## Construct and orthogonalize interaction terms:
-        if(pcAuxData$intMeth > 1) pcAuxData$computeInteract()
-
+    ## Construct and orthogonalize interaction terms:
+    if (pcAuxData$intMeth > 1) {
+      pcAuxData$computeInteract()
       write.csv(pcAuxData$data, file = "C:/Users/Danny Squire/Documents/Projects/data.csv")
       write.csv(pcAuxData$interact, file = "C:/Users/Danny Squire/Documents/Projects/interact.csv")
-
-        ## Extract the nonlinear principal component scores:
-        doPCA(map = pcAuxData)
-
-        pcAuxData$setTime("doNLinear")
-        if(pcAuxData$checkStatus == "all") pcAuxData$setStatus("doNLinear")
     }
 
-    ## Remove unnecessary representation of nominal variables:
-    pcAuxData$facNoms <- "Removed to save resources"
-    pcAuxData$dumNoms <- "Removed to save resources"
 
-    pcAuxData$setTime("rmVars")
-    if(pcAuxData$checkStatus == "all") pcAuxData$setStatus("rmVars")
 
-    pcAuxData
+    ## Extract the nonlinear principal component scores:
+    doPCA(map = pcAuxData)
+
+    pcAuxData$setTime("doNLinear")
+    if (pcAuxData$checkStatus == "all")
+      pcAuxData$setStatus("doNLinear")
+  }
+
+  ## Remove unnecessary representation of nominal variables:
+  pcAuxData$facNoms <- "Removed to save resources"
+  pcAuxData$dumNoms <- "Removed to save resources"
+
+  pcAuxData$setTime("rmVars")
+  if (pcAuxData$checkStatus == "all")
+    pcAuxData$setStatus("rmVars")
+
+  pcAuxData
 }# END createPcAux()
